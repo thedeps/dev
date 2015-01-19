9158c732826dda365a1d01959ef6e88bb5633a5d
Alex Martins <alexmbezerrajp@gmail.com>
2015-01-19 06:04:32 +0000

    atualizando para o heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 587,
+    port: 5432,
     domain: ENV["gmail.com"],
     authentication: "plain",
     enable_starttls_auto: true,
